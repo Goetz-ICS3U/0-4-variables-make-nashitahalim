e53a56510b1b2ed392bd6e420a94b372a57c5c0d
nashitahalim <354230677@educ.dpcdsb.org>
2026-02-19 12:58:01 -0500

    vars make

---
+++ vars_make.py
@@ -0,0 +1,16 @@
+"""
+author: Nashita Halim
+date: 19/2/26
+Making Variables
+"""
+
+#input
+radius = int(input(""))
+l_rect = int(input(""))
+w_rect = int(input(""))
+radius = int(input(""))
+radius = int(input(""))
+
+#processing
+
+#output
